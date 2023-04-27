0f5dc23daeb955e6ce207846adc0ee83039b1d38
Peter Bjorklund <piot@hotmail.com>
2023-04-27 20:39:13 +0200

    fix: sprite for each team

--- src/include/nimble-ball-presentation/render.h
+++ src/include/nimble-ball-presentation/render.h
@@ -8,21 +8,32 @@
 #include <sdl-render/sprite.h>
 #include <sdl-render/rect.h>
 #include <sdl-render/window.h>
+#include <sdl-render/font.h>
 
 struct NlGame;
 
+
+typedef struct NlRenderStats
+{
+    uint32_t predictedTickId;
+    int authoritativeStepsInBuffer;
+} NlRenderStats;
+
 typedef struct NlRender {
-    SrSprite avatarSprite;
+    SrSprite avatarSpriteForTeam[2];
     SrSprite ballSprite;
     SrSprites spriteRender;
     SrRects rectangleRender;
     SrWindow window;
     const struct NlGame* authoritative;
     const struct NlGame* predicted;
+    SrFont font;
+    NlRenderStats stats;
 } NlRender;
 
+
 void nlRenderInit(NlRender* self);
-void nlRenderUpdate(NlRender* self, const struct NlGame* authoritative, const struct NlGame* predicted);
+void nlRenderUpdate(NlRender* self, const struct NlGame* authoritative, const struct NlGame* predicted, const NlRenderStats stats);
 void nlRenderClose(NlRender* self);
 
 #endif
--- src/lib/render.c
+++ src/lib/render.c
@@ -7,18 +7,18 @@
 #include <nimble-ball-presentation/render.h>
 #include <nimble-ball-simulation/nimble_ball_simulation.h>
 
-static void setupAvatarSprite(SrSprite* sprite, SDL_Texture* texture)
+static void setupAvatarSprite(SrSprite* sprite, SDL_Texture* texture, int cellIndex)
 {
-    sprite->rect.x = 0;
+    sprite->rect.x = 0 + cellIndex * 21;
     sprite->rect.y = 0;
-    sprite->rect.w = 22;
-    sprite->rect.h = 32;
+    sprite->rect.w = 21;
+    sprite->rect.h = 31;
     sprite->texture = texture;
 }
 
 static void setupBallSprite(SrSprite* sprite, SDL_Texture* texture)
 {
-    sprite->rect.x = 89;
+    sprite->rect.x = 89 ;
     sprite->rect.y = 36;
     sprite->rect.w = 18;
     sprite->rect.h = 18;
@@ -32,17 +32,20 @@ void nlRenderInit(NlRender* self)
     SDL_Texture* avatarsTexture = IMG_LoadTexture(self->window.renderer, "data/avatars.png");
     SDL_Texture* equipmentTexture = IMG_LoadTexture(self->window.renderer, "data/equipment.png");
 
+    srFontInit(&self->font, self->window.renderer, "data/mouldy.ttf", 10);
+
     srSpritesInit(&self->spriteRender, self->window.renderer);
     srRectsInit(&self->rectangleRender, self->window.renderer);
-    setupAvatarSprite(&self->avatarSprite, avatarsTexture);
+    setupAvatarSprite(&self->avatarSpriteForTeam[0], avatarsTexture, 0);
+    setupAvatarSprite(&self->avatarSpriteForTeam[1], avatarsTexture, 1);
     setupBallSprite(&self->ballSprite, equipmentTexture);
 }
 
 static bl_vector2i simulationToRender(BlVector2 pos)
 {
     bl_vector2i result;
-    result.x = pos.x / 1;
-    result.y = pos.y / 1;
+    result.x = pos.x;
+    result.y = pos.y;
 
     return result;
 }
@@ -74,8 +77,7 @@ static void renderCallback(void* _self, SrWindow* _)
         const NlAvatar* avatar = &predicted->avatars.avatars[i];
         bl_vector2i avatarRenderPos = simulationToRender(avatar->circle.center);
         int degreesAngle = (int)(avatar->visualRotation*360.0f/(M_PI*2.0f));
-        CLOG_VERBOSE("degrees :%d (%f)" , degreesAngle, avatar->visualRotation);
-        srSpritesCopyEx(&self->spriteRender, &self->avatarSprite, avatarRenderPos.x, avatarRenderPos.y, degreesAngle, 1.0f);
+        srSpritesCopyEx(&self->spriteRender, &self->avatarSpriteForTeam[avatar->teamIndex], avatarRenderPos.x, avatarRenderPos.y, degreesAngle, 1.0f);
     }
 
     bl_vector2i ballRenderPos = simulationToRender(predicted->ball.circle.center);
@@ -84,14 +86,25 @@ static void renderCallback(void* _self, SrWindow* _)
 
     renderGoals(&self->rectangleRender, &g_nlConstants);
     renderBorders(&self->rectangleRender, &g_nlConstants);
+    //SDL_Color color = {0xff, 0xff, 0xff, SDL_ALPHA_OPAQUE};
+
+    SDL_SetRenderDrawColor(self->window.renderer, 0x44, 0x22, 0x44, 0x22);
+    const int borderSize = 22;
+    srRectsFillRect(&self->rectangleRender, 0, 359-borderSize, 640, borderSize);
+
+    char buf[64];
+    tc_snprintf(buf, 64, "pid %04X", self->stats.predictedTickId);
+    SDL_Color color = {0xff, 0xff, 0xff, SDL_ALPHA_OPAQUE};
+    srFontRenderAndCopy(&self->font, buf, 10, 359-6, color);
 }
 
-void nlRenderUpdate(NlRender* self, const NlGame* authoritative, const NlGame* predicted)
+void nlRenderUpdate(NlRender* self, const NlGame* authoritative, const NlGame* predicted, NlRenderStats stats)
 {
     self->authoritative = authoritative;
     self->predicted = predicted;
+    self->stats = stats;
 
-    srWindowRender(&self->window, 0xff0000, self, renderCallback);
+    srWindowRender(&self->window, 0x115511, self, renderCallback);
 }
 
 void nlRenderClose(NlRender* self)
